d3dce417366d0fe34b8ba6187666a6e87796fc8b
Markus Hoehn <markushoehn2007@gmail.com>
2024-11-09 09:38:21 -0600

    gps socket handler

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-"""Python Flask WebApp Auth0 integration example
-"""
-
 import json
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for, request, jsonify
+from flask import Flask, redirect, render_template, session, url_for
+from flask_socketio import SocketIO
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -16,6 +14,7 @@ if ENV_FILE:
 app = Flask(__name__)
 app.secret_key = env.get("APP_SECRET_KEY")
 
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 oauth = OAuth(app)
 
@@ -29,16 +28,16 @@ oauth.register(
     server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
 )
 
+user_gps_data = {}
 
-# Controllers API
 @app.route("/")
 def home():
+    user = session.get("user")
     return render_template(
         "home.html",
-        session=session.get("user"),
-        pretty=json.dumps(session.get("user"), indent=4),
+        session=user,
+        pretty=json.dumps(user, indent=4) if user else None,
     )
-
 
 @app.route("/callback", methods=["GET", "POST"])
 def callback():
@@ -46,13 +45,11 @@ def callback():
     session["user"] = token
     return redirect("/")
 
-
 @app.route("/login")
 def login():
     return oauth.auth0.authorize_redirect(
         redirect_uri=url_for("callback", _external=True)
     )
-
 
 @app.route("/logout")
 def logout():
@@ -71,11 +68,46 @@ def logout():
     )
 
 
-@app.route("/gps", methods=["POST"])
-def index():
-    data = request.get_json()
-    print(data)
-    return jsonify(data)
+@socketio.on('gps_data')
+def handle_gps_data(data):
+    """
+    Handle incoming GPS data from clients.
+    Expected data format (JSON):
+    {
+        "user_id": "<string>",
+        "latitude": <float>,
+        "longitude": <float>,
+        "timestamp": <string or int>
+    }
+    """
+    received_user_id = data.get('user_id')
+    authenticated_user = session.get("user")
+    
+    if not authenticated_user:
+        return
+    
+    session_user_id = authenticated_user.get('sub')
+    
+    if received_user_id != session_user_id:
+        return
+    
+    latitude = data.get('latitude')
+    longitude = data.get('longitude')
+    timestamp = data.get('timestamp')
+
+    if latitude is None or longitude is None or timestamp is None:
+        return
+    
+    if session_user_id not in user_gps_data:
+        user_gps_data[session_user_id] = []
+    
+    user_gps_data[session_user_id].append({
+        "latitude": latitude,
+        "longitude": longitude,
+        "timestamp": timestamp
+    })
+
+    # do some logic with the dictionary of GPS data
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=env.get("PORT", 3000))
+    socketio.run(app, host="0.0.0.0", port=int(env.get("PORT", 3000)))
